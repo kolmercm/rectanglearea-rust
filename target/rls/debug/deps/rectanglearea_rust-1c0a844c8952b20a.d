9417be2db61358677e2a50c899130bc1a2e69756
Conor <kolmercm@gmail.com>
2021-06-27 19:47:02 -0500

    Three ways to find the area of a rectangle

---
+++ target/rls/debug/deps/rectanglearea_rust-1c0a844c8952b20a.d
@@ -0,0 +1,5 @@
+/home/pi/Solana/rectanglearea-rust/target/rls/debug/deps/rectanglearea_rust-1c0a844c8952b20a.rmeta: src/main.rs
+
+/home/pi/Solana/rectanglearea-rust/target/rls/debug/deps/rectanglearea_rust-1c0a844c8952b20a.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/deps/rectanglearea_rust-963b94cbeee29856.d
@@ -0,0 +1,5 @@
+/home/pi/Solana/rectanglearea-rust/target/rls/debug/deps/rectanglearea_rust-963b94cbeee29856.rmeta: src/main.rs
+
+/home/pi/Solana/rectanglearea-rust/target/rls/debug/deps/rectanglearea_rust-963b94cbeee29856.d: src/main.rs
+
+src/main.rs:
